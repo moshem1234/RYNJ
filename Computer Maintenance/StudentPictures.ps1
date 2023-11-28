31761343384d56e66c99acf235cd09ee9095808e
moshem1234 <mmoskow1@stevens.edu>
2023-11-28 12:26:18 -0500

    Added Student Profile Pictures Script

---
+++ Computer Maintenance/StudentPictures.ps1
@@ -0,0 +1,39 @@
+$CSVData = Import-CSV -Path \\PC1380\Results\IDMapping.csv
+
+$IDEmailMapping = @{}
+ForEach ($Entry in $CSVData) {
+    $IDEmailMapping[$Entry."DDC-ID"] = $Entry."Email"
+}
+
+Function Get-Email {
+    param (
+        [string]$ID
+    )
+    If ($IDEmailMapping.ContainsKey($ID)) {
+        Return $IDEmailMapping[$ID]
+    }
+	Else {
+        Return "ID Not Found"
+    }
+}
+
+Set-Location A:\StudentPictures\Renamed
+Get-ChildItem -Name | Where-Object {$_ -Like '*(1).jpg'} | Remove-Item -Force
+$Files = Get-ChildItem -Name
+
+ForEach ($File in $Files){
+	$Total = $Files.Count
+	Write-Progress -Activity "Updating Profile Pictures" -Status "$Count/$Total" -PercentComplete (($Count / $Files.Count) * 100)
+	$DDCID = $File -Replace "[^\d]"
+	$Email = Get-Email $DDCID
+
+	If ($Email -ne "ID Not Found") {
+		gam user $Email update photo filename "A:\StudentPictures\Renamed\$File" | Out-File -FilePath C:\Results\GamStatus.log -Append
+	}
+	Else {
+		Write-Output "No Email Found - $DDCID" | Out-File C:\Results\MissingIDs.log -Append
+		# Write-Error "$DDCID - No Email Found"
+	}
+	$Count ++
+}
+
