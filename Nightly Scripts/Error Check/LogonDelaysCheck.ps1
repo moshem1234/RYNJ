2c14ebd1d3ebaed1046d783151f2d8bf24b3d434
moshem1234 <mmoskow1@stevens.edu>
2023-12-06 08:09:14 -0500

    Added string output to logindelay email

--- Nightly Scripts/Error Check/LogonDelaysCheck.ps1
+++ Nightly Scripts/Error Check/LogonDelaysCheck.ps1
@@ -3,7 +3,7 @@ $PCs = Get-Content -Path \\PC1380\Scripts\AllPCs.txt
 ForEach ($Server in $PCs) {
 	Write-Progress -Activity "Checking Logs" -Status $Server -PercentComplete (($Count / $PCs.Count) * 100)
 	If (Test-Connection -ComputerName $Server -Quiet -Count 1 -ErrorAction SilentlyContinue) {
-		$Var += Get-WinEvent -ComputerName $Server -LogName "Application" -FilterXPath "*[System[Provider[@Name='Microsoft-Windows-Winlogon'] and (Level=1  or Level=2 or Level=3 or Level=5) and (EventID=6006)]]" -ErrorAction SilentlyContinue | Select-Object @{name='ComputerName'; expression={"$Server"}}, TimeCreated, Message | Where-Object {$_.TimeCreated -GT $Date} | ForEach-Object {If ([Int]($_.Message.Replace("The winlogon notification subscriber ","").Replace(" second(s) to handle the notification event ","").Replace(" took ","").Replace(".","") -Replace "<.*?>" -Replace "\(([^\)]+)\)") -GT 180) {$_}}
+		$Var += Get-WinEvent -ComputerName $Server -LogName "Application" -FilterXPath "*[System[Provider[@Name='Microsoft-Windows-Winlogon'] and (Level=1  or Level=2 or Level=3 or Level=5) and (EventID=6006)]]" -ErrorAction SilentlyContinue | Select-Object @{name='ComputerName'; expression={"$Server"}}, TimeCreated, Message | Where-Object {$_.TimeCreated -GT $Date} | ForEach-Object {If ([Int]($_.Message.Replace("The winlogon notification subscriber ","").Replace(" second(s) to handle the notification event ","").Replace(" took ","").Replace(".","") -Replace "<.*?>" -Replace "\(([^\)]+)\)") -GT 180) {$_}} | Out-String
 	}
 		$count += 1
 }
