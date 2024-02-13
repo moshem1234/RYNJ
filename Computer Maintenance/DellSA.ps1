b5d7eef5264c9477e16ffacc26adb1d0b75288da
moshem1234 <mmoskow1@stevens.edu>
2024-02-13 11:00:25 -0500

    Updated DellSA with new version

--- Computer Maintenance/DellSA.ps1
+++ Computer Maintenance/DellSA.ps1
@@ -3,7 +3,7 @@ ForEach ($Server in $PCs) {
 	Write-Progress -Activity "Mass-Invoking" -Status $Server -PercentComplete (($Count / $PCs.Count) * 100)
 	If (Test-Connection -ComputerName $Server -Quiet -Count 1 -ErrorAction SilentlyContinue) {
 		$Version = Invoke-Command -ComputerName $Server -ScriptBlock {wmic product where "name = 'Dell SupportAssist'" get version} | Out-String
-		If ($Version -NotLike '*3.14.2.45116*') {
+		If ($Version -NotLike '*4.0.0.51819*') {
 			Write-Host $Server -ForegroundColor DarkBlue
 			Write-Host $Version
 		}
