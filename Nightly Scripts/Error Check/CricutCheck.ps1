37c107e4c2745d48e8efb5e817bd13dcb8fc717f
moshem1234 <mmoskow1@stevens.edu>
2024-04-28 10:51:18 -0400

    Minor changes to cricut check

--- Nightly Scripts/Error Check/CricutCheck.ps1
+++ Nightly Scripts/Error Check/CricutCheck.ps1
@@ -7,8 +7,8 @@ $Diff2 = $Diff | Out-String
 If ($Diff) {
 	Send-MailMessage -From "Moshe's PC <itnotifications@rynj.org>" -To '<mmoskowitz@rynj.org>' -Subject "New Cricut Version Detected" -Credential $Credential -UseSSL -SmtpServer 'smtp-relay.gmail.com' -Port 25 -body "https://design.cricut.com/ `nhttps://help.cricut.com/hc/en-us/sections/360002412713-Release-Notes`nDifferences: '$Diff2'" -WarningAction:SilentlyContinue
 	Copy-Item ReleaseNotesNew.txt ReleaseNotesOld.txt -Force
-	Write-Host "Files Different: $Diff2" -ForegroundColor DarkGreen
+	# Write-Host "Files Different: $Diff2" -ForegroundColor DarkGreen
 }
-Else {
-	Write-Host "Files Identical" -ForegroundColor DarkGreen
-}
+# Else {
+# 	Write-Host "Files Identical" -ForegroundColor DarkGreen
+# }
