647dac20b0281c2f46871bd90d273d908e3200b5
moshem1234 <mmoskow1@stevens.edu>
2024-05-27 19:56:11 -0400

    Updated individual cleanup to include request to remove profiles

--- Computer Maintenance/Cleanup-All-In-One.bat
+++ Computer Maintenance/Cleanup-All-In-One.bat
@@ -1,34 +1,48 @@
 @ECHO OFF
 
+:Prompt
+set /P Profiles=Would you like to Delete all User Profiles?[Y/N]
+if /I NOT "%Profiles%" EQU "Y" if /I NOT "%Profiles%" EQU "N" goto :Prompt
+cls
+
+:Uninstall
 echo Uninstall
 call \\PC1380\Scripts\Uninstall.bat
 timeout 100
 cls
 
+:MSStore
 echo Updating Microsoft Store Apps
 powershell \\PC1380\Scripts\MSStoreUpdate.ps1
 cls
 
+if /I "%Profiles%" EQU "N" goto :DiskCleanup
+
+:Profiles
 echo Deleting Profiles
 powershell \\PC1380\Scripts\ClearProfiles.ps1
 timeout 100
 cls
 
+:DiskCleanup
 echo Disk Cleanup
 call \\PC1380\Scripts\Disk-Cleanup_Auto.bat
 timeout 100
 cls
 
+:SFC
 echo SFC Scan
 call \\PC1380\Scripts\System-Files-Cleanup.bat
 timeout 100
 cls
 
+:DISM
 echo DISM Image Cleanup
 call \\PC1380\Scripts\Dism-Image-Cleanup.bat
 timeout 100
 cls
 
-@REM hostname >> \\PC1380\Results\Updated.txt
+:Restart
+hostname >> \\PC1380\Results\Updated.txt
 echo Restarting Computer
 powershell Restart-Computer -Force
