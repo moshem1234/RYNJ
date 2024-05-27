10bcf8f10c8260c1bfdfc308bd61cde1b02bfee2
moshem1234 <mmoskow1@stevens.edu>
2024-05-27 19:58:53 -0400

    Added DDM back to Uninstall Script

--- Computer Maintenance/Uninstall.bat
+++ Computer Maintenance/Uninstall.bat
@@ -5,6 +5,6 @@ wmic product where "Name like 'Dell Digital Delivery Services'" call uninstall
 wmic product where "Name like 'Adobe Flash Player 11 Plugin'" call uninstall
 cd "C:\Program Files\Dell\Dell Display Manager 2"
 uninst.exe /S
-cd "C:\Program Files\Dell\Dell Display Manager 2.0"
-uninst.exe /S
+@REM cd "C:\Program Files\Dell\Dell Display Manager 2.0"
+@REM uninst.exe /S
 powershell -file "\\PC1380\Scripts\OneDrive-Uninstall.ps1"
