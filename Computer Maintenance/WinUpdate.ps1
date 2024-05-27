cf2e7d60ba150937874704915faaf21c29b43571
moshem1234 <mmoskow1@stevens.edu>
2024-05-27 19:58:23 -0400

    Added Module Update to WinUpdate

--- Computer Maintenance/WinUpdate.ps1
+++ Computer Maintenance/WinUpdate.ps1
@@ -5,5 +5,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module PSWindowsUpdate
 Import-Module PSWindowsUpdate
+Update-Module PSWindowsUpdate
 Get-WindowsUpdate -AcceptAll -Install -AutoReboot
 $ProgressPreference = $OriginalPref
